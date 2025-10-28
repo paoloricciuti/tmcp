079f35f246a1546d4b0a85c5fe78076bda4a6f56
paoloricciuti <ricciutipaolo@gmail.com>
2025-10-28 22:29:28 +0100

    feat: export `InfoSessionManager` and `InMemoryInfoSessionManager` from session manager

--- packages/session-manager/src/types/index.d.ts
+++ packages/session-manager/src/types/index.d.ts
@@ -1,8 +1,12 @@
 declare module '@tmcp/session-manager' {
+    import type { Context } from 'tmcp';
+    /**
+     * @import { Context } from "tmcp";
+     */
     /**
      * @abstract
      */
-    export abstract class SessionManager {
+    export abstract class StreamSessionManager {
         /**
          * @abstract
          * */
@@ -20,14 +24,55 @@ declare module '@tmcp/session-manager' {
          * */
         abstract send(sessions: string[] | undefined, data: string): void | Promise<void>;
     }
-    export class InMemorySessionManager extends SessionManager {
+    export class InMemoryStreamSessionManager extends StreamSessionManager {
         create(id: string, controller: ReadableStreamDefaultController): void;
         delete(id: string): void;
+        has(id: string): Promise<boolean>;
+        send(sessions: string[] | undefined, data: string): void;
+        #private;
+    }
+    /**
+     * @abstract
+     */
+    export abstract class InfoSessionManager {
         /**
          * @abstract
          * */
-        abstract has(id: string): Promise<boolean>;
-        send(sessions: string[] | undefined, data: string): void;
+        abstract getClientInfo(id: string): Promise<NonNullable<Context["sessionInfo"]>["clientInfo"]>;
+        /**
+         * @abstract
+         * */
+        abstract setClientInfo(id: string, client_info: NonNullable<Context["sessionInfo"]>["clientInfo"]): void;
+        /**
+         * @abstract
+         * */
+        abstract getClientCapabilities(id: string): Promise<NonNullable<Context["sessionInfo"]>["clientCapabilities"]>;
+        /**
+         * @abstract
+         * */
+        abstract setClientCapabilities(id: string, client_capabilities: NonNullable<Context["sessionInfo"]>["clientCapabilities"]): void;
+        /**
+         * @abstract
+         * */
+        abstract getLogLevel(id: string): Promise<NonNullable<Context["sessionInfo"]>["logLevel"]>;
+        /**
+         * @abstract
+         * */
+        abstract setLogLevel(id: string, log_level: NonNullable<Context["sessionInfo"]>["logLevel"]): void;
+        /**
+         * @abstract
+         * */
+        abstract getSubscriptions(uri: string): Promise<string[]>;
+        /**
+         * @abstract
+         * */
+        abstract addSubscription(id: string, uri: string): void;
+        /**
+         * @abstract
+         * */
+        abstract delete(id: string): void;
+    }
+    export class InMemoryInfoSessionManager extends InfoSessionManager {
         #private;
     }
     export {};
