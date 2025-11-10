ccb2693f40eb44fd3b573b4b80dfe247829394e5
paoloricciuti <ricciutipaolo@gmail.com>
2025-11-10 23:20:06 +0100

    fix: add atrocious hack to satisfy enum type string required by official sdk

--- packages/adapter-arktype/src/index.js
+++ packages/adapter-arktype/src/index.js
@@ -6,6 +6,30 @@
 import { JsonSchemaAdapter } from 'tmcp/adapter';
 
 /**
+ * Atrocious hack to satisfy the current version of the protocol that for some reason
+ * requires `type: string` on enum fields despite JSON Schema spec not requiring it.
+ *
+ * TODO: Remove this once the protocol is fixed to align with JSON Schema spec.
+ * @param {JSONSchema7} json_schema
+ */
+function add_type_to_enums(json_schema) {
+	for (let key in json_schema) {
+		const property = json_schema[/** @type {keyof json_schema} */ (key)];
+		if (
+			property != null &&
+			typeof property === 'object' &&
+			!Array.isArray(property)
+		) {
+			if ('enum' in property && !('type' in property)) {
+				property.type = 'string';
+			}
+			add_type_to_enums(property);
+		}
+	}
+	return json_schema;
+}
+
+/**
  * ArkType adapter for converting ArkType schemas to JSON Schema format
  * @augments {JsonSchemaAdapter<Type>}
  */
@@ -17,6 +41,8 @@ export class ArktypeJsonSchemaAdapter extends JsonSchemaAdapter {
 	 */
 	async toJsonSchema(schema) {
 		// ArkType has built-in JSON Schema export via the toJsonSchema method
-		return /** @type {JSONSchema7} */ (schema.toJsonSchema());
+		return add_type_to_enums(
+			/** @type {JSONSchema7} */ (schema.toJsonSchema()),
+		);
 	}
 }
